bd264fc21b01862b982b2c9dc0364da710cee3c2
kushwaha-sonu <sonu9110063900@gmail.com>
2024-08-20 22:38:48 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "https://cypher-fullstack-assingment-8lcjja1zs.vercel.app",
+    origin: "https://cypher-fullstack-assingment-n18u.vercel.app",
     credentials: true,
   })
 );
